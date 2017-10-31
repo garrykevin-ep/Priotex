a2d197c4a26e1a77dac4d311a061dc3172ff3647
garrykevin <garrykevin.ep@gmail.com>
2017-10-31 18:50:51 +0530

    add del

--- core/static/core/js/index.js
+++ core/static/core/js/index.js
@@ -1,6 +1,9 @@
+function jsStr(key,data) {
+	return "'"+key+"'" + ":'"+ data + "'"
+}
 function todoTemplate(text,data)
 {
-	return '<li class="card"><div class="tick-box"><input type="checkbox" name="check"></div><div class="list-text">'+ text +'</div><div class="pull-right"><a href="#" onclick=deleteList(this,{'+"'type'" + ":'" + data.type + "'" + '}) class="no-link" >Delete</a></div></li>'
+	return '<li class="card"><div class="tick-box"><input onclick="markDone(this,{'+ jsStr("type",data.type) +"})" + '"' + 'type="checkbox" name="check"></div><div class="list-text">'+ text +'</div><div class="pull-right"><a href="#" onclick=deleteList(this,{' + jsStr("type",data.type) + '}) class="no-link" >Delete</a></div></li>'
 }
 
 
@@ -25,4 +28,14 @@ function deleteList(elm,data)
 {
 	//console.log(elm);
 	$(elm).closest('li').remove();
+}
+
+
+function markDone(elm,data)
+{
+	if(elm.checked == true)
+		data['done'] = "true"
+	else
+		data['done'] = "false"
+	// console.log("done")
 }
